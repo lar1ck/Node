526f2631933a627fd3949cbb68f7679b070e8425
lar1ck <shyakangango77@gmail.com>
2025-04-20 12:44:01 +0200

    connection to db

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+MONGO_URI = "mongodb://localhost:27017/L5SODSTUDENT"
+
+const connection = async () => {
+    try{
+        await mongoose.connect(MONGO_URI);
+        console.log("Connected to MongoDB")
+    }catch(e){
+        console.log("Connection failed", e);
+    }
+}
+
+connection();
